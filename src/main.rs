0249d8794f3bc5f18643cb9a8c2536f61a762c82
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-11-27 08:11:07 +0900

    finish 3.5

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,5 @@
 fn main() {
-    println!("Hello, world!");
-    another_function(5, 'h');
-
-    let x = five();
-    println!("The value of x is: {}", x);
-}
-
-/// この関数は引数として整数を受け取り、その値を表示します
-fn another_function(value: i32, unit_label: char) {
-    println!("The measurement is: {}{}", value, unit_label);
-}
-
-fn five() -> i32 {
-    5
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
 }
