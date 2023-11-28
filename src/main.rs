91c49684fc91bd66c18659a3f9542f0634095787
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-11-29 08:18:20 +0900

    finish 4.3

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,45 @@
 fn main() {
-    let mut s = String::from("hello");
+    let s = String::from("hello world");
 
-    {
-        change(&mut s);
-    } // r1はここでスコープを抜けるので、問題なく新しい参照を作ることができる
-
-    change(&mut s);
-    println!("{}", s);
+    let word = first_word(&s); // word will get the value 5
+    println!("The first word is: {}", word);
+    let word = second_word(&s); // word will get the value 5
+    println!("The second word is: {}", word);
 }
 
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        // iter() returns each element in a collection
+        if item == b' ' {
+            // b' ' is a byte literal
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn second_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    let mut first_space = 0;
+    let mut second_space = 0;
+
+    for (i, &item) in bytes.iter().enumerate() {
+        // iter() returns each element in a collection
+        if item == b' ' {
+            // b' ' is a byte literal
+            if first_space == 0 {
+                first_space = i;
+            } else {
+                second_space = i;
+            }
+        }
+    }
+    if (second_space == 0) {
+        return &s[first_space + 1..];
+    }
+
+    &s[first_space + 1..second_space]
 }
