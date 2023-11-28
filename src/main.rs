3f42c2f6b57fe048828c350f3ffac88f18f2c6d3
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-11-29 08:57:33 +0900

    finish 5.1

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,36 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+struct Color(i32, i32, i32); // tuple struct
+struct Point(i32, i32, i32); // tuple struct
+
 fn main() {
-    let s = String::from("hello world");
+    let user1 = build_user(
+        String::from("y.hotta.engineer@gmail.com"),
+        String::from("y-hotta"),
+    );
+    println!("user: {}", user1.email);
 
-    let word = first_word(&s); // word will get the value 5
-    println!("The first word is: {}", word);
-    let word = second_word(&s); // word will get the value 5
-    println!("The second word is: {}", word);
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        ..user1
+    };
+    println!("user: {}", user2.email);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
 
-fn first_word(s: &String) -> &str {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        // iter() returns each element in a collection
-        if item == b' ' {
-            // b' ' is a byte literal
-            return &s[0..i];
-        }
+fn build_user(email: String, username: String) -> User {
+    User {
+        email, // field init shorthand
+        username,
+        active: true,
+        sign_in_count: 1,
     }
-
-    &s[..]
-}
-
-fn second_word(s: &String) -> &str {
-    let bytes = s.as_bytes();
-    let mut first_space = 0;
-    let mut second_space = 0;
-
-    for (i, &item) in bytes.iter().enumerate() {
-        // iter() returns each element in a collection
-        if item == b' ' {
-            // b' ' is a byte literal
-            if first_space == 0 {
-                first_space = i;
-            } else {
-                second_space = i;
-            }
-        }
-    }
-    if (second_space == 0) {
-        return &s[first_space + 1..];
-    }
-
-    &s[first_space + 1..second_space]
 }
