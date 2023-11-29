14f77c1eabb84e36e0aadaad776a915157c83999
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-11-29 09:58:28 +0900

    finish 5.2

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,22 @@
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
-
-struct Color(i32, i32, i32); // tuple struct
-struct Point(i32, i32, i32); // tuple struct
 
 fn main() {
-    let user1 = build_user(
-        String::from("y.hotta.engineer@gmail.com"),
-        String::from("y-hotta"),
-    );
-    println!("user: {}", user1.email);
-
-    let user2 = User {
-        email: String::from("another@example.com"),
-        username: String::from("anotherusername567"),
-        ..user1
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
     };
-    println!("user: {}", user2.email);
 
-    let black = Color(0, 0, 0);
-    let origin = Point(0, 0, 0);
+    println!("rect1 is {:#?}", rect1);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
 }
 
-fn build_user(email: String, username: String) -> User {
-    User {
-        email, // field init shorthand
-        username,
-        active: true,
-        sign_in_count: 1,
-    }
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
