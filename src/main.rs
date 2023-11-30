dbb281a7b51fe58fc18175bba857bfa25b4a80d7
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-11-30 21:01:45 +0900

    fiinish 6.2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,47 @@
-fn main() {
-    let some_number = Some(5);
-    let some_string = Some("a string");
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
 
-    let absent_number: Option<i32> = None;
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    println!(
+        "{} {} {} {}",
+        value_in_cents(Coin::Penny),
+        value_in_cents(Coin::Nickel),
+        value_in_cents(Coin::Dime),
+        value_in_cents(Coin::Quarter(UsState::Alabama))
+    );
+
+    let option = plus_one(Some(5));
+    println!("{:?}", option);
 }
