b2cbc24cba6653e040d7c7708818b17e36f2e2ad
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-12-01 08:14:08 +0900

    finish 6.3

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 enum UsState {
     Alabama,
     Alaska,
+    // --snip--
 }
 
 enum Coin {
@@ -11,37 +12,12 @@ enum Coin {
     Quarter(UsState),
 }
 
-fn value_in_cents(coin: Coin) -> u32 {
-    match coin {
-        Coin::Penny => {
-            println!("Lucky penny!");
-            1
-        }
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {:?}!", state);
-            25
-        }
-    }
-}
-
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        None => None,
-        Some(i) => Some(i + 1),
-    }
-}
-
 fn main() {
-    println!(
-        "{} {} {} {}",
-        value_in_cents(Coin::Penny),
-        value_in_cents(Coin::Nickel),
-        value_in_cents(Coin::Dime),
-        value_in_cents(Coin::Quarter(UsState::Alabama))
-    );
-
-    let option = plus_one(Some(5));
-    println!("{:?}", option);
+    let mut count = 0;
+    let coin = Coin::Quarter(UsState::Alaska);
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
 }
