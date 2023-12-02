940d2c23268921852516ab7009e5c4ee9730aaa3
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-12-02 15:51:32 +0900

    finish 8.1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
-use std::fmt::Result;
-use std::io::Result as IoResult;
-
-use std::collections::HashMap;
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
 
 fn main() {
-    let mut map = HashMap::new();
-    map.insert(1, 2);
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element."),
+    }
+
+    for i in &v {
+        println!("{}", i);
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
 }
