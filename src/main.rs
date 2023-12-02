de31523373db4e933e6b0a695c627d24809feed7
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-12-02 17:43:48 +0900

    finish 8.3

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,41 @@
+use std::collections::HashMap;
+
 fn main() {
-    let mut s = String::from("foo");
-    s.push_str("bar");
-    println!("{}", s);
+    let mut scores: HashMap<String, i32> = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
 
-    let mut s1 = String::from("foo");
-    let s2 = "bar";
-    s1.push_str(s2);
-    println!("s2 is {}", s2);
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+    println!("{:?}", scores);
 
-    let s1 = String::from("Hello, ");
-    let s2 = String::from("world!");
-    let s3 = s1 + &s2;
-    println!("{}", s3);
+    let score = scores.get(&String::from("Blue"));
+    match score {
+        Some(v) => println!("score: {}", v),
+        None => println!("score: None"),
+    }
 
-    let s1 = String::from("tic");
-    let s2 = String::from("tac");
-    let s3 = String::from("toe");
-    let s = format!("{}-{}-{}", s1, s2, s3);
-    println!("{}, {}, {}, {}", s, s1, s2, s3);
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
 
-    let len = String::from("Hola").len();
-    println!("{}", len);
-    let len = String::from("Здравствуйте").len();
-    let s = "Здравствуйте";
-    println!("{}, {}", len, &s[0..4]);
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+    println!("{:?}", scores);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+    println!("{:?}", scores);
+
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", map);
 }
