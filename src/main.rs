46be1c34541e33d13e58cf68088b9517688b5e9b
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-12-03 10:20:56 +0900

    finish 9.1

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,5 @@
-use std::collections::HashMap;
-
 fn main() {
-    let mut scores: HashMap<String, i32> = HashMap::new();
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
+    let v = vec![1, 2, 3];
 
-    let teams = vec![String::from("Blue"), String::from("Yellow")];
-    let initial_scores = vec![10, 50];
-    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
-    println!("{:?}", scores);
-
-    let score = scores.get(&String::from("Blue"));
-    match score {
-        Some(v) => println!("score: {}", v),
-        None => println!("score: None"),
-    }
-
-    for (key, value) in &scores {
-        println!("{}: {}", key, value);
-    }
-
-    let mut scores = HashMap::new();
-
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Blue"), 25);
-    println!("{:?}", scores);
-
-    scores.entry(String::from("Yellow")).or_insert(50);
-    scores.entry(String::from("Blue")).or_insert(50);
-    println!("{:?}", scores);
-
-    let text = "hello world wonderful world";
-    let mut map = HashMap::new();
-
-    for word in text.split_whitespace() {
-        let count = map.entry(word).or_insert(0);
-        *count += 1;
-    }
-    println!("{:?}", map);
+    v[99];
 }
