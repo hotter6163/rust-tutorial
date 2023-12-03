96281b157a478dee58e161392d73ae3c9a97bcb8
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-12-03 18:01:10 +0900

    finish 10.3

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,23 @@
-mod summary;
+#[derive(Debug)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
 
-use summary::{NewsArticle, Summary, Tweet};
+impl<'a> ImportantExcerpt<'a> {
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        //       "お知らせします: {}"
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
 
 fn main() {
-    let tweet = Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from(
-            // もちろん、ご存知かもしれませんがね、みなさん
-            "of course, as you probably already know, people",
-        ),
-        reply: false,
-        retweet: false,
+    let novel = String::from("Call me Ishmael. Some years ago...");
+
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
     };
 
-    println!("1 new tweet: {}", tweet.summarize());
-    notify(&tweet);
-
-    let article = NewsArticle {
-        // ペンギンチームがスタンレーカップチャンピオンシップを勝ち取る！
-        headline: String::from("Penguins win the Stanley Cup Championship!"),
-        // アメリカ、ペンシルベニア州、ピッツバーグ
-        location: String::from("Pittsburgh, PA, USA"),
-        // アイスバーグ
-        author: String::from("Iceburgh"),
-        // ピッツバーグ・ペンギンが再度NHL(National Hockey League)で最強のホッケーチームになった
-        content: String::from(
-            "The Pittsburgh Penguins once again are the best \
-             hockey team in the NHL.",
-        ),
-    };
-
-    println!("New article available! {}", article.summarize());
-    notify(&article);
-
-    let number_list = vec![34, 50, 25, 100, 65];
-
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
-}
-
-fn largest<T: PartialOrd>(list: &[T]) -> &T {
-    let mut largest = &list[0];
-
-    for item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
-fn notify(item: &impl Summary) {
-    println!("Breaking news! {}", item.summarize());
+    println!("{:?}", i);
 }
