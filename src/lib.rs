2ab688f95122a32a78bb8cf5a543c5e6bf783196
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-12-04 21:20:31 +0900

    finish 11.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,9 @@
-// https://doc.rust-jp.rs/book-ja/ch11-02-running-tests.html#%E5%90%8D%E5%89%8D%E3%81%A7%E3%83%86%E3%82%B9%E3%83%88%E3%81%AE%E4%B8%80%E9%83%A8%E3%82%92%E5%AE%9F%E8%A1%8C%E3%81%99%E3%82%8B
-
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn can_hold(&self, other: &Self) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-}
-
 pub fn add_two(a: i32) -> i32 {
     a + 2
 }
 
-pub fn greeting(name: &str) -> String {
-    format!("Hello {}!", name)
-}
-
-pub struct Guess {
-    value: i32,
-}
-
-impl Guess {
-    pub fn new(value: i32) -> Guess {
-        if value < 1 {
-            panic!(
-                //予想値は1以上でなければなりませんが、{}でした。
-                "Guess value must be greater than or equal to 1, got {}.",
-                value
-            );
-        } else if value > 100 {
-            panic!(
-                //予想値は100以下でなければなりませんが、{}でした。
-                "Guess value must be less than or equal to 100, got {}.",
-                value
-            );
-        }
-
-        Guess { value }
-    }
-}
-
-fn prints_and_returns_10(a: i32) -> i32 {
-    //{}を返す
-    println!("I got the value {}", a);
-    10
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
 }
 
 #[cfg(test)]
@@ -55,78 +11,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn exploration() {
-        assert_eq!(2 + 2, 4);
-    }
-
-    #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle {
-            width: 10,
-            height: 10,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 5,
-        };
-
-        assert!(larger.can_hold(&smaller));
-    }
-
-    #[test]
-    fn smaller_cannot_hold_larger() {
-        let larger = Rectangle {
-            width: 10,
-            height: 10,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 5,
-        };
-
-        assert!(!smaller.can_hold(&larger));
-    }
-
-    #[test]
-    fn add_two_and_two() {
-        assert_eq!(4, add_two(2));
-    }
-
-    #[test]
-    fn add_three_and_two() {
-        assert_eq!(5, add_two(3));
-    }
-
-    #[test]
-    fn one_hundred() {
-        assert_eq!(102, add_two(100));
-    }
-
-    #[test]
-    fn greeting_contains_name() {
-        let result = greeting("Carol");
-        assert!(
-            result.contains("Carol"),
-            "Greeting did not contain name, value was `{}`",
-            result
-        );
-    }
-
-    #[test]
-    #[should_panic(expected = "Guess value must be less than or equal to 100")]
-    fn greater_than_100() {
-        Guess::new(200);
-    }
-
-    #[test]
-    fn this_test_will_pass() {
-        let value = prints_and_returns_10(4);
-        assert_eq!(10, value);
-    }
-
-    #[test]
-    fn this_test_will_fail() {
-        let value = prints_and_returns_10(8);
-        assert_eq!(5, value);
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
     }
 }
