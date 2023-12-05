4d3fc1c7b9038d7a97083568190a496d311a2d30
yoshikazu hotta <95465875+hotter6163@users.noreply.github.com>
2023-12-06 08:39:44 +0900

    finish 12.4

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        // 引数の解析に失敗しました
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
     if let Err(e) = rust_tutorial::run(config) {
-        // アプリケーションエラー: {}
         println!("Application error: {}", e);
-
         process::exit(1);
     }
 }
