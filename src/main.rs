5b48c34f497bcf61c01f38b3d8c5c3fd9f25ce1c
yoshikazu hotta <95465875+hotter6163@users.noreply.github.com>
2023-12-05 10:32:36 +0900

    finish 12.2

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,24 @@
-fn main() {}
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let query = args.get(1).expect("Please provide a first argument");
+    let filename = args.get(2).expect("Please provide a second argument");
+
+    let contents = read_file_contents(filename);
+    println!("With text:\n{}", contents);
+}
+
+fn read_file_contents(filename: &str) -> String {
+    println!("In file: {}", filename);
+
+    let mut f = File::open(filename).expect("file not found");
+    let mut contents = String::new();
+
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    contents
+}
--- tests/integration_test.rs
+++
@@ -1,9 +0,0 @@
-extern crate rust_tutorial;
-
-mod common;
-
-#[test]
-fn it_adds_two() {
-    common::setup();
-    assert_eq!(4, rust_tutorial::add_two(2));
-}
