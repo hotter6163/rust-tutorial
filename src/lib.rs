c5f0fd39e72a2aed0af923bafba310e09aea3f4a
yoshikazu hotta <95465875+hotter6163@users.noreply.github.com>
2023-12-09 08:18:15 +0900

    finish 13.2

--- src/lib.rs
+++ src/lib.rs
@@ -104,3 +104,107 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
 
     results
 }
+
+// --------------------
+
+#[derive(Debug, PartialEq)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == size).collect()
+}
+
+#[cfg(test)]
+mod test_2 {
+    use super::*;
+
+    #[test]
+    fn iterator_demonstration() {
+        let v1 = vec![1, 2, 3];
+        let mut vi_iter = v1.iter();
+
+        assert_eq!(vi_iter.next(), Some(&1));
+        assert_eq!(vi_iter.next(), Some(&2));
+        assert_eq!(vi_iter.next(), Some(&3));
+        assert_eq!(vi_iter.next(), None);
+    }
+
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
+        let vi_iter = v1.iter();
+
+        let total: i32 = vi_iter.sum();
+
+        assert_eq!(total, 6);
+    }
+
+    #[test]
+    fn map() {
+        let v1: Vec<i32> = vec![1, 2, 3];
+        let v2: Vec<i32> = v1.iter().map(|x| x + 1).collect();
+
+        assert_eq!(v2, vec![2, 3, 4]);
+    }
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+
+        let in_my_size = shoes_in_my_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe {
+                    size: 10,
+                    style: String::from("sneaker")
+                },
+                Shoe {
+                    size: 10,
+                    style: String::from("boot")
+                },
+            ]
+        );
+    }
+}
+
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@
 
 // use std::env;
 // use std::process;
-use std::thread;
-use std::time::Duration;
 
 // use rust_tutorial::Config;
 
@@ -21,74 +19,17 @@ use std::time::Duration;
 //     }
 // }
 
-// https://doc.rust-jp.rs/book-ja/ch13-01-closures.html#%E9%96%A2%E6%95%B0%E3%81%A7%E3%83%AA%E3%83%95%E3%82%A1%E3%82%AF%E3%82%BF%E3%83%AA%E3%83%B3%E3%82%B0
+// --------------------
+
 fn main() {
-    let simulated_user_specified_value = 10;
-    let simulated_random_number = 7;
+    let v1 = vec![1, 2, 3];
+    let mut vi_iter = v1.iter();
 
-    generate_workout(simulated_user_specified_value, simulated_random_number);
+    assert_eq!(vi_iter.next(), Some(&1));
+    assert_eq!(vi_iter.next(), Some(&2));
+    assert_eq!(vi_iter.next(), Some(&3));
+    assert_eq!(vi_iter.next(), None);
 
-    let x = 4;
-
-    let equal_rt_x = |z| z == x;
-
-    let y = 4;
-    assert!(equal_rt_x(y));
-
-    let x = vec![1, 2, 3];
-
-    let equal_to_x = move |z| z == x;
-
-    println!("can't use x here: {:?}", x);
-
-    let y = vec![1, 2, 3];
-
-    assert!(equal_to_x(y));
-}
-
-struct Cacher<T: Fn(u32) -> u32> {
-    calculation: T,
-    value: Option<u32>,
-}
-
-impl<T: Fn(u32) -> u32> Cacher<T> {
-    fn new(calculation: T) -> Cacher<T> {
-        Cacher {
-            calculation,
-            value: None,
-        }
-    }
-
-    fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
-            None => {
-                let v = (self.calculation)(arg);
-                self.value = Some(v);
-                v
-            }
-        }
-    }
-}
-
-fn generate_workout(intensity: u32, random_number: u32) {
-    let mut expensive_retult = Cacher::new(|num| {
-        println!("calculating slowly...");
-        thread::sleep(Duration::from_secs(2));
-        num
-    });
-
-    if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_retult.value(intensity));
-        println!("Next, do {} situps!", expensive_retult.value(intensity));
-    } else {
-        if random_number == 3 {
-            println!("Take a break today! Remember to stay hydrated!");
-        } else {
-            println!(
-                "Today, run for {} minutes!",
-                expensive_retult.value(intensity)
-            );
-        }
-    }
+    let v1: Vec<i32> = vec![1, 2, 3];
+    let v2: Vec<i32> = v1.iter().map(|x| x + 1).collect();
 }
