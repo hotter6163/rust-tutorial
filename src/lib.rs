3fba21613fb5c29ee24d4b39425bfa9c5b12ab1d
yoshikazu hotta <95465875+hotter6163@users.noreply.github.com>
2023-12-09 10:09:31 +0900

    finish 14.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,99 +1,30 @@
-use std::env;
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
+//! # My Crate
+//!
+//! `my_crate` is a collection of utilities to make performing certain
+//! calculations more convenient.
 
-#[cfg(test)]
-mod test {
-    use super::*;
+//! #自分のクレート
+//!
+//! `my_crate`は、ユーティリティの集まりであり、特定の計算をより便利に行うことができます。
 
-    #[test]
-    fn case_sensitive() {
-        let query = "duct";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Duct tape.";
-
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
-    }
-
-    #[test]
-    fn case_insensitive() {
-        let query = "rUsT";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
-
-        assert_eq!(
-            vec!["Rust:", "Trust me."],
-            search_case_insensitive(query, contents)
-        );
+pub mod func {
+    /// Adds one to the number given.
+    /// 与えられた数値に1を足す。
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let five = 5;
+    ///
+    /// assert_eq!(6, rust_tutorial::func::add_one(5));
+    /// ```
+    pub fn add_one(x: i32) -> i32 {
+        x + 1
     }
 }
 
-pub struct Config {
-    pub query: String,
-    pub filename: String,
-    pub case_sensitive: bool,
-}
+mod art;
 
-impl Config {
-    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
-        args.next();
-
-        let query = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get a query string."),
-        };
-        let filename = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get a filename string."),
-        };
-
-        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
-
-        Ok(Config {
-            query,
-            filename,
-            case_sensitive,
-        })
-    }
-}
-
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let mut f = File::open(config.filename)?;
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
-
-    let result = if config.case_sensitive {
-        search(&config.query, &contents)
-    } else {
-        search_case_insensitive(&config.query, &contents)
-    };
-
-    for line in result {
-        println!("{}", line);
-    }
-
-    Ok(())
-}
-
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    contents
-        .lines()
-        .filter(|line| line.contains(query))
-        .collect()
-}
-
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    contents
-        .lines()
-        .filter(|line| line.to_lowercase().contains(&query))
-        .collect()
-}
+pub use art::kinds::PrimaryColor;
+pub use art::kinds::SecondaryColor;
+pub use art::utils;
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,8 @@
 extern crate rust_tutorial;
 
-use std::env;
-use std::process;
-
-use rust_tutorial::Config;
+use rust_tutorial::func;
 
 fn main() {
-    let config = Config::new(env::args()).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
-
-    if let Err(e) = rust_tutorial::run(config) {
-        eprintln!("Application error: {}", e);
-        process::exit(1);
-    }
+    println!("Hello, world!");
+    println!("1 + 1 = {}", func::add_one(1));
 }
