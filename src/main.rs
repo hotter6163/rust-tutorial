2c2493163d3550146c2ca24755cc821e36a744f6
yoshikazu hotta <95465875+hotter6163@users.noreply.github.com>
2023-12-13 08:37:30 +0900

    finish 17.3

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,14 @@
-mod gui;
-
-use gui::{Button, Draw, Screen};
-
-#[derive(Debug)]
-struct SelectBox {
-    width: u32,
-    height: u32,
-    options: Vec<String>,
-}
-
-impl Draw for SelectBox {
-    fn draw(&self) {
-        println!("Drawing a select box: {:?}", self);
-    }
-}
+mod blog;
+use blog::Post;
 
 fn main() {
-    let screen = Screen {
-        components: vec![
-            Box::new(SelectBox {
-                width: 75,
-                height: 10,
-                options: vec![
-                    String::from("Yes"),
-                    String::from("Maybe"),
-                    String::from("No"),
-                ],
-            }),
-            Box::new(Button {
-                width: 50,
-                height: 10,
-                label: String::from("OK"),
-            }),
-        ],
-    };
+    let mut post = Post::new();
 
-    screen.run();
+    post.add_text("I ate a salad for lunch today");
+
+    let post = post.request_review();
+
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
