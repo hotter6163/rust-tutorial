eb7c02d7afffae500dad0af8d9ce6fff246230ad
yoshikazu hotta <95465875+hotter6163@users.noreply.github.com>
2023-12-13 09:22:59 +0900

    finish 18.1

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
-mod blog;
-use blog::Post;
-
 fn main() {
-    let mut post = Post::new();
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
 
-    post.add_text("I ate a salad for lunch today");
+    if let Some(color) = favorite_color {
+        // あなたのお気に入りの色、{}を背景色に使用します
+        println!("Using your favorite color, {}, as the background", color);
+    } else if is_tuesday {
+        // 火曜日は緑の日！
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            // 紫を背景色に使用します
+            println!("Using purple as the background color");
+        } else {
+            // オレンジを背景色に使用します
+            println!("Using orange as the background color");
+        }
+    } else {
+        // 青を背景色に使用します
+        println!("Using blue as the background color");
+    }
 
-    let post = post.request_review();
+    let point = (3, 5);
+    print_coordinates(&point);
+}
 
-    let post = post.approve();
-
-    assert_eq!("I ate a salad for lunch today", post.content());
+fn print_coordinates(&(x, y): &(i32, i32)) {
+    // 現在の位置: ({}, {})
+    println!("Current location: ({}, {})", x, y);
 }
