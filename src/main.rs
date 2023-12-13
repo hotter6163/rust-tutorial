c9c25ca7170db79341bc5ed88185edc6e51f6845
yoshikazu hotta <95465875+hotter6163@users.noreply.github.com>
2023-12-13 22:27:10 +0900

    finish 19.1

--- src/main.rs
+++ src/main.rs
@@ -1,121 +1,44 @@
-struct Point {
-    x: i32,
-    y: i32,
-}
-
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 }, // 匿名構造体
-    Write(String),
-    ChangeColor(i32, i32, i32), // タプル構造体
+extern "C" {
+    fn abs(input: i32) -> i32;
 }
 
 fn main() {
-    let x = Some(5);
-    let y = 10;
+    let mut num = 5;
 
-    match x {
-        Some(50) => println!("Got 50"),
-        Some(y) => println!("Matched, y = {:?}", y),
-        _ => println!("Default case, x = {:?}", x),
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
     }
 
-    println!("At the end: x = {:?}, y = {:?}", x, y);
+    let mut v = vec![1, 2, 3, 4, 5, 6];
 
-    let x = 5;
+    let r = &mut v[..];
 
-    match x {
-        1..=5 => println!("one through five"),
-        // それ以外
-        _ => println!("something else"),
-    }
+    let (a, b) = split_at_mut(r, 3);
 
-    let x = 'c';
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
 
-    match x {
-        // ASCII文字前半
-        'a'..='j' => println!("early ASCII letter"),
-        // ASCII文字後半
-        'k'..='z' => println!("late ASCII letter"),
-        // それ以外
-        _ => println!("something else"),
-    }
-
-    let p = Point { x: 0, y: 7 };
-    let Point { x, y } = p;
-    println!("a = {}, b = {}", x, y);
-
-    let p = Point { x: 0, y: 7 };
-
-    match p {
-        Point { x, y: 0 } => println!("On the x axis at {}", x),
-        Point { x: 0, y } => println!("On the y axis at {}", y),
-        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
-    }
-
-    let msg = Message::ChangeColor(0, 160, 255);
-
-    match msg {
-        Message::Quit => {
-            println!("The Quit variant has no data to destructure.")
-        }
-        Message::Move { x, y } => {
-            println!("Move in the x direction {} and in the y direction {}", x, y);
-        }
-        Message::Write(text) => println!("Text message: {}", text),
-        Message::ChangeColor(r, g, b) => {
-            println!("Change the color to red {}, green {}, and blue {}", r, g, b);
-        }
-    }
-
-    let points = vec![
-        Point { x: 0, y: 0 },
-        Point { x: 1, y: 5 },
-        Point { x: 10, y: -3 },
-    ];
-
-    let sum_of_squares: i32 = points.iter().map(|&Point { x, y }| x * x + y * y).sum();
-    println!("{}", sum_of_squares);
-
-    let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
-    println!("feet = {}, inches = {}, x = {}, y = {}", feet, inches, x, y);
-
-    foo(3, 4);
-
-    let mut setting_value = Some(5);
-    let new_setting_value = Some(10);
-
-    match (setting_value, new_setting_value) {
-        (Some(_), Some(_)) => {
-            println!("Can't overwrite an existing customized value");
-        }
-        _ => {
-            setting_value = new_setting_value;
-        }
-    }
-
-    println!("setting is {:?}", setting_value);
-
-    let robot_name = Some(String::from("Bors"));
-
-    match robot_name {
-        Some(ref name) => println!("Found a name: {}", name),
-        None => (),
-    }
-
-    println!("robot_name is: {:?}", robot_name);
-
-    let num = Some(4);
-
-    match num {
-        Some(x) if x < 5 => println!("less than five: {}", x),
-        Some(x) => println!("{}", x),
-        None => (),
+    unsafe {
+        println!("Absolute value of -3 according to C: {}", abs(-3));
     }
 }
 
-// https://doc.rust-jp.rs/book-ja/ch18-03-pattern-syntax.html#%E3%83%8D%E3%82%B9%E3%83%88%E3%81%95%E3%82%8C%E3%81%9F_%E3%81%A7%E5%80%A4%E3%81%AE%E4%B8%80%E9%83%A8%E3%82%92%E7%84%A1%E8%A6%96%E3%81%99%E3%82%8B
+use std::slice;
 
-fn foo(_: i32, y: i32) {
-    println!("This code only uses the y parameter: {}", y);
+fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = slice.len();
+    let ptr = slice.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.offset(mid as isize), len - mid),
+        )
+    }
 }
